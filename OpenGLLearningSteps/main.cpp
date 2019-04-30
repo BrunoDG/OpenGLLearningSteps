b72ed1c2fc38831de6586ae7d6bab704ee48ea56
Bruno Dias Gonçalves <brunodg@tecgraf.puc-rio.br>
2019-04-30 18:22:36 -0300

    Created vertex shader, creating fragment shader (Tutorial 2)

--- OpenGLLearningSteps/main.cpp
+++ OpenGLLearningSteps/main.cpp
@@ -14,6 +14,8 @@
 //Include GLM
 #include <glm/glm.hpp>
 
+//include the 
+
 GLFWwindow* window; // (In the Accompanying source code, this variable is global for simplicity)
 
 int main() 
@@ -58,10 +60,46 @@ int main()
 	// Set the background as a dark blue color
 	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
 
+	GLuint VertexArrayId;
+	glGenVertexArrays(1, &VertexArrayId);
+	glBindVertexArray(VertexArrayId);
+
+	// An Array of 3 vectors which represents 3 vertices
+	static const GLfloat g_vertex_buffer_data[] = {
+		-1.0f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f
+	};
+
+	// This will idenfity our vertex buffer
+	GLuint vertexBuffer;
+	// Generate 1 buffer, put the resulting identifier in vertexBuffer
+	glGenBuffers(1, &vertexBuffer);
+	// The following commands will talk about our 'vertexbuffer' buffer
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+	// Give our vertices to OpenGL
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
 	do 
 	{
 		// Clear the screen. It's not mentioned before Tutorial 02, but it can cause flilckering, so it's there nonetheless
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		// 1st attribute buffer: vertices
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+		glVertexAttribPointer(
+			0,			// attribute 0. No particular reason for 0, but must match the layout in the shader.
+			3,			// size
+			GL_FLOAT,	// type
+			GL_FALSE,	// is it normalized? if yes, then GL_TRUE. Otherwise, GL_FALSE
+			0,			// stride
+			(void*)0	// array buffer offset
+		);
+
+		// Draw the triangle, finally!
+		glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+		glDisableVertexAttribArray(0);
 
 		// Swap buffers
 		glfwSwapBuffers(window);
@@ -71,6 +109,8 @@ int main()
 	// Check if thw ESC key was pressed or the window was closed
 	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
 
+
 	// Close OpenGL window and terminate GLFW
 	glfwTerminate();
+
 }
